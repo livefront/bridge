e179ea5e80fcf5fc1d09c20731fe689b280e7372
Brian Yencho <brian@livefront.com>
2019-05-29 14:30:08 -0500

    Add basic scenarios for large data in Fragments

--- bridgesample/src/main/java/com/livefront/bridgesample/main/model/Scenario.kt
+++ bridgesample/src/main/java/com/livefront/bridgesample/main/model/Scenario.kt
@@ -2,8 +2,11 @@ package com.livefront.bridgesample.main.model
 
 import android.content.Context
 import com.livefront.bridgesample.R
+import com.livefront.bridgesample.scenario.activity.FragmentContainerActivity
 import com.livefront.bridgesample.scenario.activity.LargeDataActivity
 import com.livefront.bridgesample.scenario.activity.NonBridgeLargeDataActivity
+import com.livefront.bridgesample.scenario.fragment.LargeDataFragment
+import com.livefront.bridgesample.scenario.fragment.NonBridgeLargeDataFragment
 
 /**
  * This is the list of all Bridge scenarios demonstrated in this app.
@@ -18,5 +21,21 @@ fun getScenarios(context: Context): List<MainItem> = listOf(
                 R.string.non_bridge_large_data_scenario_title,
                 R.string.non_bridge_large_data_scenario_description,
                 NonBridgeLargeDataActivity.getNavigationIntent(context)
+        ),
+        MainItem(
+                R.string.large_data_fragment_scenario_title,
+                R.string.large_data_fragment_scenario_description,
+                FragmentContainerActivity.getNavigationIntent(
+                        context,
+                        LargeDataFragment.getFragmentData()
+                )
+        ),
+        MainItem(
+                R.string.non_bridge_large_data_fragment_scenario_title,
+                R.string.non_bridge_large_data_fragment_scenario_description,
+                FragmentContainerActivity.getNavigationIntent(
+                        context,
+                        NonBridgeLargeDataFragment.getFragmentData()
+                )
         )
 )
---
+++ bridgesample/src/main/java/com/livefront/bridgesample/scenario/fragment/LargeDataFragment.kt
@@ -0,0 +1,76 @@
+package com.livefront.bridgesample.scenario.fragment
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.evernote.android.state.State
+import com.livefront.bridgesample.R
+import com.livefront.bridgesample.base.BridgeBaseFragment
+import com.livefront.bridgesample.scenario.activity.FragmentData
+import com.livefront.bridgesample.scenario.activity.SuccessActivity
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_large_data.bitmapGeneratorView
+
+class LargeDataFragment : BridgeBaseFragment() {
+    override val shouldClearOnDestroy: Boolean
+        get() = getArguments(this).shouldClearOnDestroy
+
+    @State
+    var savedBitmap: Bitmap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_large_data, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bitmapGeneratorView.apply {
+            setHeaderText(R.string.large_data_header)
+            generatedBitmap = savedBitmap
+            onBitmapGeneratedListener = { savedBitmap = it }
+            onNavigateButtonClickListener = {
+                startActivity(Intent(context!!, SuccessActivity::class.java))
+            }
+        }
+    }
+
+    companion object {
+        private const val ARGUMENTS_KEY = "arguments"
+
+        fun getArguments(
+            fragment: LargeDataFragment
+        ): LargeDataArguments = fragment
+                .arguments!!
+                .getParcelable(ARGUMENTS_KEY)
+
+        fun getFragmentData(
+            largeDataArguments: LargeDataArguments = LargeDataArguments()
+        ) = FragmentData(
+                R.string.large_data_screen_title,
+                LargeDataFragment::class.java,
+                getInitialArguments(largeDataArguments)
+        )
+
+        fun getInitialArguments(
+            largeDataArguments: LargeDataArguments = LargeDataArguments()
+        ) = Bundle().apply {
+            putParcelable(ARGUMENTS_KEY, largeDataArguments)
+        }
+
+        fun newInstance(
+            largeDataArguments: LargeDataArguments = LargeDataArguments()
+        ) = LargeDataFragment().apply {
+            arguments = getInitialArguments(largeDataArguments)
+        }
+    }
+}
+
+@Parcelize
+data class LargeDataArguments(
+    val shouldClearOnDestroy: Boolean = true
+) : Parcelable
