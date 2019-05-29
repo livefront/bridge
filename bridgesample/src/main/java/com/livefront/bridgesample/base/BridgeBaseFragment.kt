df4bf37f48e8d47d3c3876e9b6342033851bb4b5
Brian Yencho <brian@livefront.com>
2019-05-29 15:26:07 -0500

    Add ability to disable Bridge clearing in onDestroy

--- bridgesample/src/main/java/com/livefront/bridgesample/base/BridgeBaseFragment.kt
+++ bridgesample/src/main/java/com/livefront/bridgesample/base/BridgeBaseFragment.kt
@@ -2,9 +2,17 @@ package com.livefront.bridgesample.base
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
 import com.livefront.bridge.Bridge
 
 abstract class BridgeBaseFragment : Fragment() {
+    /**
+     * Determines whether or not [Bridge.clear] will be called in [onDestroy]. This is enabled by
+     * default but may be disabled in scenarios where Fragments are "recycled" (such as when using
+     * a [FragmentStatePagerAdapter].
+     */
+    open val shouldClearOnDestroy: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Bridge.restoreInstanceState(this, savedInstanceState)
@@ -17,6 +25,6 @@ abstract class BridgeBaseFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Bridge.clear(this)
+        if (shouldClearOnDestroy) Bridge.clear(this)
     }
 }
