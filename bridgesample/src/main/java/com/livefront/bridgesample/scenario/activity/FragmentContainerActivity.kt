da3c54492a7b67da81c35f18aa265379bd735036
Brian Yencho <brian@livefront.com>
2019-05-29 14:26:42 -0500

    Add an Activity that can launch a single Fragment

---
+++ bridgesample/src/main/java/com/livefront/bridgesample/scenario/activity/FragmentContainerActivity.kt
@@ -0,0 +1,74 @@
+package com.livefront.bridgesample.scenario.activity
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import android.support.annotation.StringRes
+import android.support.v4.app.Fragment
+import android.view.MenuItem
+import com.livefront.bridgesample.R
+import com.livefront.bridgesample.base.BridgeBaseActivity
+import com.livefront.bridgesample.scenario.activity.FragmentContainerActivity.Companion.getNavigationIntent
+import com.livefront.bridgesample.util.handleHomeAsBack
+import com.livefront.bridgesample.util.setHomeAsUpToolbar
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.basic_toolbar.toolbar
+
+/**
+ * A simple [Activity] with a container in which an initial [Fragment] may be placed. This is
+ * done by supplying [FragmentData] via the [getNavigationIntent] method.
+ */
+class FragmentContainerActivity : BridgeBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_container)
+
+        val data = getFragmentData(this)
+        setHomeAsUpToolbar(toolbar, data.titleRes)
+
+        if (savedInstanceState != null) return
+
+        val fragment = Fragment.instantiate(
+                this,
+                data.fragmentClass.name,
+                data.fragmentBundle
+        )
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = handleHomeAsBack(item) {
+        super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val FRAGMENT_DATA_KEY = "data-key"
+
+        fun getFragmentData(
+            activity: FragmentContainerActivity
+        ): FragmentData = activity
+                .intent
+                .getParcelableExtra(FRAGMENT_DATA_KEY)
+
+        fun getNavigationIntent(
+            context: Context,
+            fragmentData: FragmentData
+        ) = Intent(context, FragmentContainerActivity::class.java).apply {
+            putExtra(FRAGMENT_DATA_KEY, fragmentData)
+        }
+    }
+}
+
+/**
+ * Used to determine the initial [Fragment] to show in a [FragmentContainerActivity].
+ */
+@Parcelize
+data class FragmentData(
+    @StringRes val titleRes: Int,
+    val fragmentClass: Class<out Fragment>,
+    val fragmentBundle: Bundle?
+) : Parcelable
