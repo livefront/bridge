8e3f0b516896cd8148409d713a8cbe4cd2bddacd
Brian Yencho <brian@livefront.com>
2019-05-30 10:55:28 -0500

    Allow the sample app to save the state of Views

--- bridgesample/src/main/java/com/livefront/bridgesample/app/BridgeSampleApplication.kt
+++ bridgesample/src/main/java/com/livefront/bridgesample/app/BridgeSampleApplication.kt
@@ -2,9 +2,12 @@ package com.livefront.bridgesample.app
 
 import android.app.Application
 import android.os.Bundle
+import android.os.Parcelable
+import android.view.View
 import com.evernote.android.state.StateSaver
 import com.livefront.bridge.Bridge
 import com.livefront.bridge.SavedStateHandler
+import com.livefront.bridge.ViewSavedStateHandler
 
 class BridgeSampleApplication : Application() {
     override fun onCreate() {
@@ -19,6 +22,17 @@ class BridgeSampleApplication : Application() {
                     override fun restoreInstanceState(target: Any, state: Bundle?) {
                         StateSaver.restoreInstanceState(target, state)
                     }
+                },
+                object : ViewSavedStateHandler {
+                    override fun <T : View> saveInstanceState(
+                        target: T,
+                        parentState: Parcelable?
+                    ): Parcelable = StateSaver.saveInstanceState(target, parentState)
+
+                    override fun <T : View> restoreInstanceState(
+                        target: T,
+                        state: Parcelable?
+                    ): Parcelable? = StateSaver.restoreInstanceState(target, state)
                 }
         )
     }
